c2e8f8a523e148d7980f6a2950d7b99de827fe33
max <din.chan@mavs.uta.edu>
2021-05-15 22:20:42 -0500

    Initial Commit

---
+++ SparkJoy/SparkJoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkJoyApp.swift
+//  SparkJoy
+//
+//  Created by Max on 5/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SparkJoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
